0fcfb7b6f5122b135208a4a302d18a0bc248c889
Tejashree3 <dhalgadetejashree100@gmail.com>
2025-07-23 15:49:49 +0530

    Update Contactus.jsx

--- src/component/Contactus.jsx
+++ src/component/Contactus.jsx
@@ -113,8 +113,7 @@ const Contactus = () => {
             transition={{ duration: 0.8 }}
           >
             <iframe
-src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3779.500715095125!2d73.79428477519566!3d18.6863879824396!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTjCsDQxJzExLjAiTiA3M8KwNDcnNDguNyJF!5e0!3m2!1sen!2sin!4v1753265337230!5m2!1sen!2sin"              width="600"
-              height="450"
+src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3779.500715095125!2d73.79428477519566!3d18.6863879824396!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTjCsDQxJzExLjAiTiA3M8KwNDcnNDguNyJF!5e0!3m2!1sen!2sin!4v1753265337230!5m2!1sen!2sin" 
               allowfullscreen=""
               loading="lazy"
               className="rounded-md border"
